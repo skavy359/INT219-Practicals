2a273da1ff0304fe104039ba20edf36fa8a811a5
Kavy Sharma <skavy359@gmail.com>
2025-01-31 10:43:32 +0530

    Added some dependencies

---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"], // Adjust based on your file structure
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
