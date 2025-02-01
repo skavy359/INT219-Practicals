57083dc0e5a093bea2f8946c79e039e39172a47e
Kavy Sharma <skavy359@gmail.com>
2025-02-01 17:39:11 +0530

    deleted some files

--- Tailwind/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./*.{html,js}"], // Adjust based on your file structure
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
-
