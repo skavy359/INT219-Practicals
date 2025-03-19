cdc8cdff09864dbd0b749bb66b1d4df316180d59
Kavy Sharma <skavy359@gmail.com>
2025-03-19 18:40:18 +0530

    Learnt some basics of JavaScript

---
+++ Tailwind/src/JS-4.js
@@ -0,0 +1,44 @@
+const readlineSync = require("readline-sync");
+
+// const name = readlineSync.question("Enter your name: ");
+// const age = readlineSync.question("Enter your age: ");
+// const country = readlineSync.question("Enter your country: ");
+
+// console.log("Name: ", name);
+// console.log("Age: ", age);
+// console.log("Country: ", country);
+
+// function test(){
+//     const localVar="I am inside a function";
+//     console.log(localVar);
+// }
+
+// test();
+// console.log(localVar); Error Aayega isme local defined nahi hai bahar
+
+// if(true){
+//     let blockLet="I exist inside this block";
+//     const blockConst="Me too";
+//     var blockVar="Let's check for Var";
+//     console.log(blockLet);
+//     console.log(blockConst);
+//     console.log(blockVar);
+// }
+
+// console.log(blockLet);
+// console.log(blockConst);
+// console.log(blockVar);
+
+// const globalVar="I am Global";
+// function test(){
+//     console.log(globalVar);
+// }
+// test();
+// console.log(globalVar);
+
+// console.log(a);
+// const a = 20;
+// console.log(b);
+// var b = 20;
+// console.log(c);
+// const c = 20;
---
+++ Tailwind/src/JS-5.js
@@ -0,0 +1,20 @@
+// Arithmetic Operators
+console.log("Arithmetic Operators:");
+
+let a = 10, b = 5;
+
+console.log("a + b is", a + b);   // Addition
+console.log("a - b is", a - b);   // Subtraction
+console.log("a * b is", a * b);   // Multiplication
+console.log("a / b is", a / b);   // Division
+console.log("a % b is", a % b);   // Modulus (Remainder)
+console.log("a ** b is", a ** b); // Exponentiation (ES6 feature)
+
+// Increment and Decrement
+console.log("++a is", ++a);       // Pre-increment
+console.log("a++ is", a++);       // Post-increment
+console.log("Value of a after a++ is", a);
+
+console.log("--b is", --b);       // Pre-decrement
+console.log("b-- is", b--);       // Post-decrement
+console.log("Value of b after b-- is", b);
